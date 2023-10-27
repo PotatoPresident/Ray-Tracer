1365e14ce2bbeca18e86cd808385d50c530337d5
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2023-10-27 13:21:11 -0700

    Add dielectric material

--- material.h
+++ material.h
@@ -53,4 +53,40 @@ private:
     double fuzz;
 };
 
+class Dielectric : public Material {
+public:
+    Dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    bool scatter(const Ray& ray_in, const HitRecord& record, Color& attenuation, Ray& scattered) const override{
+        attenuation = Color(1.0, 1.0, 1.0);
+        double refraction_ratio = record.front_face ? (1.0 / ir) : ir;
+
+        Vec3 unit_direction = unitVector(ray_in.direction());
+        double cos_theta = fmin(dot(-unit_direction, record.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        Vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > randomDouble()) {
+            direction = reflect(unit_direction, record.normal);
+        } else {
+            direction = refract(unit_direction, record.normal, refraction_ratio);
+        }
+
+        scattered = Ray(record.point, direction);
+        return true;
+    }
+
+private:
+    double ir; // Index of refraction
+
+    static double reflectance(double cosine, double i_of_ref) {
+        // Use Schlick's approximation for reflectance
+        auto r0 = (1 - i_of_ref) / (1 + i_of_ref);
+        r0 = r0*r0;
+        return r0 + (1-r0)*pow((1 - cosine), 5);
+    }
+};
+
 #endif //RAYTRACER_MATERIAL_H
